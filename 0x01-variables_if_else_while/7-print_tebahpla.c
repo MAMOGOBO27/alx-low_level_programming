748cf8f4e11cdd0983ec23c280952eaf2f3c283e
MAMOGOBO27 <mamogobosn@gmail.com>
2023-03-17 19:17:32 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void)
+
+{
+	char lower;
+          for (lower = 'z'; lower >= 'a'; lower--)
+        putchar(lower);
+         putchar('\n');
+
+           return (0);
+}
+
