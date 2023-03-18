b2883fde9fc59bf77c4521d42bfcc29a647d9feb
MAMOGOBO27 <mamogobosn@gmail.com>
2023-03-17 18:48:10 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void)
+
+{ 
+	int f;
+
+	for (f = '0'; f <= '9'; f++)
+        putchar(f);
+	putchar('\n');
+
+	return (0);
+}
