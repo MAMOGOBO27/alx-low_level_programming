70fa7ffa713235d26b4969923dcbf1fec74be01e
MAMOGOBO27 <mamogobosn@gmail.com>
2023-03-17 18:26:06 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 ( success)
+ */
+
+int main(void)
+{
+	int k;
+        for (k =  0; k < 10; k++)
+	printf("%d", k);
+	printf("\n");
+
+	return (0);
+}
