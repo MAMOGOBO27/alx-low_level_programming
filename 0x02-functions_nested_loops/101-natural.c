d4eed443aa54e1498b28fd9c312d6b3da2f1a391
MAMOGOBO27 <mamogobosn@gmail.com>
2023-03-22 15:59:52 -0700

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of all multiple of 3 or 5 upto 1021
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int g, w = 0;
+
+	while (g < 1024)
+	{
+	if ((g % 3 == 0) || (g % 5 == 0))
+	{
+	w = w + i;
+	}
+	g++;
+	}
+	printf("%d\n", w);
+	return (0);
+}
