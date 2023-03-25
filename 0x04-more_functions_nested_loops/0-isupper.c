6a755a856dd6a460e3122b48a5da65b9f3396b56
MAMOGOBO27 <mamogobosn@gmail.com>
2023-03-24 18:44:54 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * Return: 1 for upper leetr or 0 for any else
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90 )
+	{
+	return (1);
+	}
+	return (0);
+}
