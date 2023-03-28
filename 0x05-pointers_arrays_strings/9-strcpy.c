ecfbb78484a749905a8a389ec641acb49005e437
MAMOGOBO27 <mamogobosn@gmail.com>
2023-03-28 07:13:19 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcy - Copy a string
+ * @dest: Destination value 
+ * @src: Source value
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int m;
+for (m = 0; src[m] != '\0'; m++)
+{
+dest[m] = src[i];
+}
+dest[m++] = '\0';
+return (dest);
+}
