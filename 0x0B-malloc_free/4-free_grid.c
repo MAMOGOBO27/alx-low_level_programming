966cd17132c63a5bad96c2fea812d9552bc449d6
MAMOGOBO27 <mamogobosn@gmail.com>
2023-04-12 01:15:48 -0700

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - ...
- * @grid: ...
- * @height: ...
- * Return: ...
+ * free_grid - frees the memory allocate for th grid
+ * @grid: grid to free
+ * @height: height of the grid
+ * Return: grid
  */
 void free_grid(int **grid, int height)
 {
         int i;
+	
+	if (grid ==NULL || height == 0)
+	return;
 
         for (i = 0; i < height; i++)
         {
-        free(grid[i]);
+      	  free(grid[i]);
         }
         free(grid);
 }
