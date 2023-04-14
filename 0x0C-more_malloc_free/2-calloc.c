13cbaaa09f82d4094e0e775707467e708afbf547
MAMOGOBO27 <mamogobosn@gmail.com>
2023-04-14 02:34:52 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,7 +5,6 @@
  * @nmemb: number of members
  * @size: size
  * Return: Nothing
- *
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -19,8 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(l);
 
 	if (p == NULL)
-	return (NULL);
-	
+	return (NULL)
 	while (i < l)
 	{
 	p[i] = 0;
