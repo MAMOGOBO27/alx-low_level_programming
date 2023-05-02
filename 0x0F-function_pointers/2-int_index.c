c83e5801d933e6d4cda22e6c31d2299749e4660a
MAMOGOBO27 <mamogobosn@gmail.com>
2023-05-02 01:17:59 -0700

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - eturns the index of the first element
+ * @array: number of elements in the array
+ * @size: number of elements in the array
+ * @cmp:  is a pointer to the function to be used to compare values
+ * Return: 0 success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+	if (array != NULL && cmp != NULL)
+	{
+	while (i < size)
+	{
+	if (cmp(array[i]))
+	return (i);
+	i++;
+	}
+	}
+	}
+	return (-1)
+}
