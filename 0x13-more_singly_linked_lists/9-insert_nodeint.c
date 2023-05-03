35a653f958054a7f88431784b73a5b0b67134e49
MAMOGOBO27 <mamogobosn@gmail.com>
2023-05-03 05:51:23 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,7 @@
  * *insert_nodeint_at_index - inserts a new node in a linked list
  * at a given position
  * @head: pointer to the first node in the list
- * @idx index where the new node is added
+ * @idx: index where the new node is added
  * @n: data to insert in the new node
  *
  * Return: pointer to the new node, or NULL
@@ -43,5 +43,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	temp = temp->next;
 	}
 	return (NULL);
-	}
-
+}
