0a97193b1618d67b4e5d8872586d9ca81f8176fe
MAMOGOBO27 <mamogobosn@gmail.com>
2023-05-04 00:30:49 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,8 @@
 #include "main.h"
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
  * *string_nconcat - Concatenates two strings of any size
  * @s1: the first string to concatenate
@@ -10,39 +12,39 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int i = 0, j = 0, k = 0, l = 0;
-        char *str;
+unsigned int i = 0, j = 0, k = 0, l = 0;
+char *str;
 
-        if (s1 == NULL)
-        s1 = "";
-        if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-        while (s1[i])
-        i++;
-        while (s2[k])
-        k++;
+while (s1[i])
+i++;
+while (s2[k])
+k++;
 
-	if (n >= k)
-	l = i + k;
-	else
-        l = i + n;
-        str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-        return (NULL);
+if (n >= k)
+l = i + k;
+else
+l = i + n;
+str = malloc(sizeof(char) * l + 1);
+if (str == NULL)
+return (NULL);
 
-        k = 0;
-        while (j < l)
-        {
-        if (j <= i)
-        str[j] = s1[j];
-        if (j >= i)
-        {
-        str[j] = s2[k];
-        k++;
-        }
-        j++;
-        }
-        str[j] = '\0';
-        return (str);
+k = 0;
+while (j < l)
+{
+if (j <= i)
+str[j] = s1[j];
+if (j >= i)
+{
+str[j] = s2[k];
+k++;
+}
+j++;
+}
+str[j] = '\0';
+return (str);
 }
