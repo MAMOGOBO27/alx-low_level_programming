b1c5a44a58969202655d47350f41d4e1f484f85c
MAMOGOBO27 <mamogobosn@gmail.com>
2023-05-06 05:25:12 -0700

    Eleven

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * set_bit - sets the bit to index 1
+ * @n: Number to pointer
+ * @index: is the index, starting from 0 of the bit you want to get
+ *
+ * Return: 1 (success), -1 (failure)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+return (-1);
+
+*n = ((1UL << index) | *n);
+return (1);
+}
