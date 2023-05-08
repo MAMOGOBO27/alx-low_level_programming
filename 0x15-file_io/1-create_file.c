e3a3d155632903cf5716d5f5b2f4a053a48e6706
MAMOGOBO27 <mamogobosn@gmail.com>
2023-05-08 12:38:01 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	len++;
 	}
 
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
